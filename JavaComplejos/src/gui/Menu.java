6d2e17d8bfd3e2ca79d043ddaae4dd8ecfcad3f4
carlosjesuspm <permarcar96@gmail.com>
2022-04-11 12:26:58 +0200

    Actualizado

--- JavaComplejos/src/gui/Menu.java
+++ JavaComplejos/src/gui/Menu.java
@@ -13,8 +13,8 @@ public class Menu {
 
         System.out.println("1. Mostrar los empleados");
         System.out.println("2. Añadir empleado");
-        System.out.println("3. Añadir empleado");
-        System.out.println("4. Añadir empleado");
+        System.out.println("3. Añadir director");
+        System.out.println("4. Añadir gerente");
         System.out.println("5. Modificar datos empleado");
         System.out.println("6. Eliminar empleado");
         System.out.println("7. Incentivar director/gerente");

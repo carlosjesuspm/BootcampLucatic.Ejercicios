b6c0b1c57ea1f12990e3fed722291da85ed436f1
carlosjesuspm <permarcar96@gmail.com>
2022-04-13 20:48:49 +0200

    Hasta Ejercicio 5

--- Testing/01_TDD/src/model/StringCalculator.java
+++ Testing/01_TDD/src/model/StringCalculator.java
@@ -2,4 +2,18 @@ package model;
 
 public class StringCalculator {
 
+	public static int Add(String numbers) {
+		int returnValue=0;
+		String[] numbersArray = numbers.split(",");
+		//if(numbersArray.length>2) {
+		//	throw new RuntimeException("Up to 2 numbers separated by comma (,), are allowed");
+		//} else {
+		for (String number : numbersArray) {
+			if (!number.trim().isEmpty()) {
+				returnValue+=Integer.parseInt(number);
+			}
+		}
+		//}
+		return returnValue;
+	}
 }
--- Testing/01_TDD/src/testStringCalculator.java
+++ Testing/01_TDD/src/testStringCalculator.java
@@ -1,12 +1,72 @@
-import static org.junit.jupiter.api.Assertions.*;
-
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+
+
+import model.StringCalculator;
 
 class testStringCalculator {
 
+	
+	// Requisito 1: El método puede tomar 0, 1 o 2 números separados por coma 
+	
+	RuntimeException exception;
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public final void WhenMoreThan2NumbersAreUsedTheExceptionIsThrown() {
+		
+		exception = Assert.assertThrows(RuntimeException.class,()->{
+			StringCalculator.Add("1,2,3");
+		});
+		Assert.assertTrue(exception.getMessage().contains("RuntimeException"));
+		
 	}
 
+	@Test
+	public final void when2NumbersAreUsedThenNotExceptionIsThrown() {
+		StringCalculator.Add("1,2");
+		Assert.assertTrue(true);
+	}
+	
+	@Test
+	public final void WhenNoNumbersIsUsedTheExceptionIsThrown() {
+		
+		exception = Assert.assertThrows(RuntimeException.class,()->{
+			StringCalculator.Add("1,x");
+		});
+		Assert.assertTrue(exception.getMessage().contains("RuntimeException"));
+	}
+	
+	
+	// Requisito 2: Para una cadena vacía el método devolverá 0. 
+	
+	@Test
+	public final void WhenEmptyStringIsUsedThenReturnValueIs0() {
+		
+		Assert.assertEquals(0, StringCalculator.Add(""));
+	}
+	
+	
+	// Requisito 3: el método devolverá la suma de los números
+	
+	@Test
+	
+	public final void WhenOneNumberIsUsedThenReturnValueIsThatSameNumber() {
+		Assert.assertEquals(1, StringCalculator.Add("1"));
+		
+	}
+	
+	
+	@Test
+	public final void WhenTwoNumberIsUsedThenReturnValueIsTheirSum() {
+		Assert.assertEquals(1+2, StringCalculator.Add("1,2"));
+	}
+	
+	
+	// Requisito 4: Permitir que el método Add() soporte una cantidad desconocida de números
+	
+	@Test
+	public final void WhenTheAddMethodHandleAnKnownAmountOfNumbers() {
+		Assert.assertEquals(1+2+3+4+5+6+7+8, StringCalculator.Add("1,2,3,4,5,6,7,8"));
+	}	
+	
 }
